739cd94fc73fbbe1c92d109f3c2ef0891e31911f
Shannon Thouvenin <sthoovy@gmail.com>
2023-04-30 19:59:56 -0400

    Added additional pieces

--- main.js
+++ main.js
@@ -1,6 +1,23 @@
 let dieRolls = [];
+let total = 0;
 
 document.getElementById("rollDice").addEventListener('click', function() {
-    let diceNumInput = document.getElementById("numberOfDice");
-    console.log(diceNumInput);
+    let numOfDice = document.getElementById("numberOfDice").value;
+    console.log(numOfDice);
+    dieRolls = [];
+    for (let i = 0; i < numOfDice; i++) {
+      let rollResult = Math.ceil(Math.random() * 6);
+      dieRolls.push(rollResult);
+      total += rollResult;
+      // document.getElementById("showAllRows").innerHTML = `<li>${rollResult}</li>`;
+      console.log(dieRolls);
+    }
+    document.getElementById("total").innerHTML = total;
 });
+
+document.getElementById("showAllRows").addEventListener('click', function() {
+  for (let i = 0; i < dieRolls; i++) {
+    console.log(dieRolls);
+    document.getElementById("listOfAllRows").innerHTML = `<li>${dieRolls[i]}</li>`;
+  }
+})

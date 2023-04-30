eb3f68185b33be1ad13857fd8a6e12f52e612fc0
Shannon Thouvenin <sthoovy@gmail.com>
2023-04-30 19:26:04 -0400

    Added eventListener

---
+++ main.js
@@ -0,0 +1,6 @@
+let dieRolls = [];
+
+document.getElementById("rollDice").addEventListener('click', function() {
+    let diceNumInput = document.getElementById("numberOfDice");
+    alert(diceNumInput);
+});

9dd35693c6d4c63604dbba0802cc0b0242bc41b9
Shannon Thouvenin <sthoovy@gmail.com>
2023-04-30 19:28:59 -0400

    adjusting event listener

--- main.js
+++ main.js
@@ -2,5 +2,5 @@ let dieRolls = [];
 
 document.getElementById("rollDice").addEventListener('click', function() {
     let diceNumInput = document.getElementById("numberOfDice");
-    alert(diceNumInput);
+    console.log(diceNumInput);
 });

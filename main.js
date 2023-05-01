4ed333d0fc6533145978e593c591dfe37b450bf5
Shannon Thouvenin <sthoovy@gmail.com>
2023-04-30 20:27:02 -0400

    Removed console.logs

--- main.js
+++ main.js
@@ -3,24 +3,22 @@ let total = 0;
 
 document.getElementById("rollDice").addEventListener('click', function() {
     let numOfDice = document.getElementById("numberOfDice").value;
-    console.log(numOfDice);
-    // dieRolls = [];
+    dieRolls = [];
+    total = 0;
+    
     for (let i = 0; i < numOfDice; i++) {
       let rollResult = Math.ceil(Math.random() * 6);
       dieRolls.push(rollResult);
       total += rollResult;
-      
     }
-    console.log(dieRolls);
+
     document.getElementById("total").innerHTML = total;
 });
 
 document.getElementById("showAllRollsButton").addEventListener('click', function() {
-    console.log("HELLO!"); 
     let listOfRolls = document.getElementById("listOfAllRolls");
+    listOfRolls.innerHTML = "";
     for (let j = 0; j < dieRolls.length; j++) {
-       console.log(dieRolls);
         listOfRolls.innerHTML += `<li>${dieRolls[j]}</li>`;
     }
-
 });

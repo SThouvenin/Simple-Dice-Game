6592c0033c90e652fd19a25260ce2ea32cab442b
Shannon Thouvenin <sthoovy@gmail.com>
2023-04-30 20:22:59 -0400

    Added server lite to the JSON package. Dice Game  is entirely functional

--- main.js
+++ main.js
@@ -4,20 +4,23 @@ let total = 0;
 document.getElementById("rollDice").addEventListener('click', function() {
     let numOfDice = document.getElementById("numberOfDice").value;
     console.log(numOfDice);
-    dieRolls = [];
+    // dieRolls = [];
     for (let i = 0; i < numOfDice; i++) {
       let rollResult = Math.ceil(Math.random() * 6);
       dieRolls.push(rollResult);
       total += rollResult;
-      // document.getElementById("showAllRows").innerHTML = `<li>${rollResult}</li>`;
-      console.log(dieRolls);
+      
     }
+    console.log(dieRolls);
     document.getElementById("total").innerHTML = total;
 });
 
-document.getElementById("showAllRows").addEventListener('click', function() {
-  for (let i = 0; i < dieRolls; i++) {
-    console.log(dieRolls);
-    document.getElementById("listOfAllRows").innerHTML = `<li>${dieRolls[i]}</li>`;
-  }
-})
+document.getElementById("showAllRollsButton").addEventListener('click', function() {
+    console.log("HELLO!"); 
+    let listOfRolls = document.getElementById("listOfAllRolls");
+    for (let j = 0; j < dieRolls.length; j++) {
+       console.log(dieRolls);
+        listOfRolls.innerHTML += `<li>${dieRolls[j]}</li>`;
+    }
+
+});
